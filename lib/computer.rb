3a3ed4a34588c142f8c2effd1b30b4beab9f9e6b
Tony Robertson <tonyr729@gmail.com>
2018-10-10 13:02:32 -0600

    Add generate coordinates method

--- lib/computer.rb
+++ lib/computer.rb
@@ -15,7 +15,7 @@ class Computer
   def get_coordinates(ship_length, previous_ship = false)
     direction = [:H, :V][rand(2)]
     letters = ["A","B","C","D"]
-
+    
     if direction == :H
       letter = letters[rand(4)]
       number_constraint = @board.size - ship_length + 1
@@ -23,11 +23,30 @@ class Computer
     else
       number = rand(4) + 1
       letter_constraint = @board.size - ship_length
-      letters = letters[rand(letter_constraint)]
+      letter = letters[rand(letter_constraint)]
     end
-    coordinate = ["#{letter}#{number}"]
+    # if previous_ship validate cord
+    
     # create method to generate following coordinates
+    generate_coordinates(letter, number, direction, ship_length)
   end
+  
+  def generate_coordinates(letter, number, direction, ship_length)
+    letters = ["A","B","C","D"]
+    letter_start_index = letters.index(letter)
+    coordinates = ["#{letter}#{number}"]
+    (1..ship_length - 1).each do |i|
+      if direction == :H
+        number += 1
+        coordinates << "#{letter}#{number}"
+      else
+        letter = letters[letter_start_index += 1]
+        coordinates << "#{letter}#{number}"
+      end
+    end
+    coordinates
+  end
+  
 end
 
 board = Board.new
