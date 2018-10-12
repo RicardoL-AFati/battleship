7366f1f060d038e7c62df9aa29f148a2c58ddc1f
Tony Robertson <tonyr729@gmail.com>
2018-10-12 12:09:16 -0600

    Add tests for validate placement in player

--- test/player_test.rb
+++ test/player_test.rb
@@ -16,12 +16,31 @@ class PlayerTest < Minitest::Test
     assert_instance_of Board, @player.board
   end
 
-  def test_it_can_validate_player_placement_choice
-    choice = "B2 B3 B4"
-    
-    assert_equal true, @player.validate_choice(choice)
+  def test_it_can_validate_placement_choice
+    choice = "B2 B3 B4"  
+    assert_equal ["B2", "B3", "B4"], @player.valid_choice?(choice)
   end
 
+  def test_it_returns_false_if_placement_is_overboard
+    bad_choice = "B3 B4 B5"
+
+    refute @player.valid_choice?(bad_choice) 
+  end
+
+  def test_it_can_validate_placement_doesnt_overlap
+    @player.board.create_ship(['A1', 'B1'])
+    choice = "A2 A3 A4"
+
+    assert_equal ["A2", "A3", "A4"], @player.valid_choice?(choice) 
+  end
+
+  def test_it_returns_false_if_placement_overlaps_previous_ship
+    @player.board.create_ship(['A1', 'B1'])
+    bad_choice = "A1 A2 A3"
+
+    refute @player.valid_choice?(bad_choice) 
+  end
+  
   def test_it_can_place_ships
     skip
     #integration test
