91002ded5922d1230d9aca4a94a0d87e5e08dea6
Ricardo Ledesma <rickydem24@gmail.com>
2018-10-12 10:54:32 -0600

    Adds invalid prompt

--- lib/prompts.rb
+++ lib/prompts.rb
@@ -1,4 +1,5 @@
 module Prompts
   WELCOME = "Welcome to BATTLESHIP!\n"
   START = "Would you like to (p)lay, read the (i)nstructions, or (q)uit?\n"
+  INVALID_CHOICE = "Enter one of the letters in () or the entire word - no spaces"
 end
