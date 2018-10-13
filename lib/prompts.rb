7086a4e3a0c857f4ff96fc179936d779be92966b
Ricardo Ledesma <rickydem24@gmail.com>
2018-10-13 13:26:07 -0600

    Adds boat hit and miss prompts

--- lib/prompts.rb
+++ lib/prompts.rb
@@ -1,26 +1,26 @@
 module Prompts
   WELCOME = "Welcome to BATTLESHIP!\n"
   START = "Would you like to (p)lay, read the (i)nstructions, or (q)uit?\n"
-  INSTRUCTIONS = 
+  INSTRUCTIONS =
   """
-  The basic Battleship rules and instructions for playing the game 
-  are each player calls out one shot (or coordinate) each turn in an 
-  attempt to hit one of their opponent’s ships. To “hit” one of your 
-  opponent’s ships, you must input a letter and a number of where you 
-  think one of their ships is located. Once a shot is called, the 
-  opponent calls out “hit” or “miss.” If one of your ships gets hit, 
-  an H will appear on your ship. If shooting at the enemy an H mark 
-  will be displayed (if a hit was made) or an M (a miss) on your target 
-  grid located. This will help you keep track of your hits and misses 
+  The basic Battleship rules and instructions for playing the game
+  are each player calls out one shot (or coordinate) each turn in an
+  attempt to hit one of their opponent’s ships. To “hit” one of your
+  opponent’s ships, you must input a letter and a number of where you
+  think one of their ships is located. Once a shot is called, the
+  opponent calls out “hit” or “miss.” If one of your ships gets hit,
+  an H will appear on your ship. If shooting at the enemy an H mark
+  will be displayed (if a hit was made) or an M (a miss) on your target
+  grid located. This will help you keep track of your hits and misses
   in your hunt to find their ships.
 
-  Once all coordinates of a ship have been filled with H, the ship has 
-  sunk and must be removed from the ocean. You then announce which ship 
-  has sunk. The Battleship rules on successfully sinking a ship are as 
-  follows:  Cruiser – 3 hits, Destroyer – 2 hits. 
+  Once all coordinates of a ship have been filled with H, the ship has
+  sunk and must be removed from the ocean. You then announce which ship
+  has sunk. The Battleship rules on successfully sinking a ship are as
+  follows:  Cruiser – 3 hits, Destroyer – 2 hits.
 
   Proper coordinate input:
-  
+
   A3 A4
 
   - LETTERS A-D
@@ -34,8 +34,10 @@ module Prompts
   TOP = "===========\n. 1 2 3 4\n"
   BOTTOM = "===========\n"
   EMPTY_BOARD = "A          \nB          \nC          \nD          \n"
+  BOAT_HIT = "Boat Hit! \u{1F4A5}\u{1F4A5}\u{1F4A5}"
+  BOAT_MISS = "Boat Miss! \u{1F4A6}\u{1F4A6}\u{1F4A6}"
   def self.print_empty_board
-    print TOP 
+    print TOP
     print EMPTY_BOARD
     print BOTTOM
   end
